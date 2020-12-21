22bf4b14fb37c77deb9bdfef37bedfede009d48c
Ben London <32783398+0bLondon@users.noreply.github.com>
2020-12-21 11:13:19 -0500

    Update run.sh

--- base_env/run.sh
+++ base_env/run.sh
@@ -1,1 +1,1 @@
-python3 pacman.py -p ExperimentalAgent -x 100 -t 100 -n 2001 -l mediumClassic -g ExpGhost -k 2 --frameTime=0.001
+python3 pacman.py -p ExperimentalAgent -x 100 -t 100 -n 205 -l mediumClassic -g ExpGhost -k 2 --frameTime=0.01
